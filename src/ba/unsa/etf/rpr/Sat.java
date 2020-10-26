574e126d6e4c2771ad5cd6ea5a4a5771ea7061f4
fmaric1 <fmaric1@etf.unsa.ba>
2020-10-26 15:44:09 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    int sati,minute,sekunde;
+    public Sat(int sati, int minute, int sekunde){
+        postavi(sati,  minute, sekunde);
+    }
+    public void postavi(int sati, int minute, int sekunde){
+        this.sati=sati;
+        this.minute=minute;
+        this.sekunde=sekunde;
+    }
+    void sljedeci() {
+        sekunde++;
+        if (sekunde == 60) { sekunde = 0; minute++; }
+        if (minute == 60) { minute = 0; sati++; }
+        if (sati == 24) sati = 0;
+    }
+    void prethodni() {
+        sekunde--;
+        if (sekunde == -1) { sekunde = 59; minute--; }
+        if (minute == -1) { minute = 59; sati--; }
+        if (sati == -1) sati = 23;
+    }
+
+    void pomjeriZa(int pomak) {
+        if (pomak > 0) for (int i = 0; i < pomak; i++) sljedeci();
+		else for (int i = 0; i<-pomak; i++) prethodni();
+    }
+    int dajSate(){
+        return sati;
+    }
+    int dajMinute(){
+        return minute;
+    }
+    int dajSekunde(){
+        return sekunde;
+    }
+    void ispisi(){
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+
+}
